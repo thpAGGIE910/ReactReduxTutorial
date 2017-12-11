017cef667cb9910d61b0ac33fb292f2d15f32ffd
Travis Payne <thpAGGIE910@gmail.com>
2017-12-11 11:09:50 -0600

    Update the SearchBar component to handle input change events

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class SearchBar extends Component {
     render() {
-        return <input />;
+        return <input onChange={evt => console.log(evt.target.value)} />;
     }
 }
 
