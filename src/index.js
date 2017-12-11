abacfd0d0d9ac61cc928573c5d7faf04dab0e813
Travis Payne <thpAGGIE910@gmail.com>
2017-12-11 13:50:26 -0600

    Add a VideoList functional component to the app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
 const API_KEY = "AIzaSyAvsx-IA44Jm_14Svgp_K7Stq83VbqNgH0";
 
@@ -25,6 +26,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
